307a2356e7300af9a4fd0eb41561bec6fb28e262
Eva <EvanesceExotica@gmail.com>
2022-02-13 16:51:09 -0500

    handling image upload

--- api/controllers/uploadsController.js
+++ api/controllers/uploadsController.js
@@ -1,7 +1,17 @@
+const path = require("path");
+
 const fileUpload = require("express-fileupload");
 const uploadImage = async (req, res) => {
     // console.log(req);
-    console.log("Files are", req.files);
+    const slideImage = req.files.image; //get the image data from the upload
+    const imagePath = path.join(__dirname, "../public/uploads/" + `${slideImage.name}`);
+    //get the path we want to move it to in tue public folder
+    await slideImage.mv(imagePath);
+    //move the image to the image path
+
+    //send the data back
+    return res.status(200).json({ image: { src: `/uploads/${slideImage.name}` } });
+
     res.status(200).send("upload product image");
 };
 
