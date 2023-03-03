7515ae8a1184ec2d2888bed72776bfe3e4188ca7
Eva <EvanesceExotica@gmail.com>
2023-03-02 20:45:33 -0500

    remove old passages, update

--- api/app.js
+++ api/app.js
@@ -21,7 +21,8 @@ const port = process.env.PORT || 9000;
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set('view engine', 'pug')
+// app.set("view engine", "jade");
 
 app.use(cors());
 app.use(logger("dev"));
@@ -36,37 +37,23 @@ app.use(express.static(path.join(__dirname, "public")));
 // });
 
 app.use(fileUpload()); //! HAD TO PUT THIS BEFORE THE APP.USE() ROUTER
-// app.use('/.netlify/functions/api', factoryRouter)
-// app.use('/.netlify/functions/api', displayRouter)
-// app.use('/.netlify/functions/api', linkInterfaceRouter)
+
 app.use("/factory", factoryRouter);
 app.use("/display", displayRouter);
 app.use("/links", linkInterfaceRouter);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
-// app.use("/users", usersRouter);
-// app.use("/testAPI", testAPIRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
 });
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, '../stretches-site/build/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../stretches-site/build/index.html'));
 });
 
 
-// // error handler
-// app.use(function (err, req, res, next) {
-//     // set locals, only providing error in development
-//     res.locals.message = err.message;
-//     res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//     // render the error page
-//     res.status(err.status || 500);
-//     res.render("error");
-// });
 
 const start = async () => {
     try {
