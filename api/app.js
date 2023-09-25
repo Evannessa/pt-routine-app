d7166a4f4b265a4a11d1921f9ce96a7b59004bbd
Eva <EvanesceExotica@gmail.com>
2023-09-25 14:49:37 -0400

    fix id issues

--- api/app.js
+++ api/app.js
@@ -5,7 +5,7 @@ const express = require("express");
 
 const app = express();
 
-require("express-async-errors");
+// require("express-async-errors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
