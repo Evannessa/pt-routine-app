3e62e5d4b299e0194242699cd7226c84cec6bc87
EvanesceExotica <EvanesceExotica@gmail.com>
2023-12-13 16:50:03 -0500

    fix exercise popup cards

--- stretches-site/src/components/ExercisePopupCard.jsx
+++ stretches-site/src/components/ExercisePopupCard.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import FloatingToolbar from './FloatingToolbar';
 import styled from 'styled-components';
-
+import hourglassPrimary from "../images/hourglass_1_full.png"
+import hourglassSecondary from "../images/hourglass_4.png"
 
 const StyledPopupCard = styled.section`
     p{
@@ -18,11 +19,13 @@ const StyledPopupCard = styled.section`
  */
 const ExercisePopupCard = (props) => {
     const { src, cannotLoad, actions, dataKey, coords, timer } = props.data
+    const placeholder = timer.isBreak ? hourglassSecondary : hourglassPrimary
+    console.log(" Source is ", src)
     return (
         <StyledPopupCard>
             <p className="tooltip-card-title">{timer.label}</p>
             <FloatingToolbar actions={actions} timerId={dataKey} coords={coords}></FloatingToolbar>
-            <img src={src} alt="Exercise Slide" crossOrigin="true" onError={({ currentTarget }) => {
+            <img src={src.length > 0 ? src : placeholder} alt="Exercise Slide" crossOrigin="true" onError={({ currentTarget }) => {
                 currentTarget.onerror = null; // prevents looping
                 currentTarget.src = cannotLoad;
             }} />
