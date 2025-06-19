2a550cc6c4e88eb1c6262d2bef18000857e2134b
EvanesceExotica <EvanesceExotica@gmail.com>
2025-06-19 19:08:32 -0400

    add routine classes/interfaces

---
+++ stretches-site/src/models/routine.tsx
@@ -0,0 +1,85 @@
+export interface Time {
+    hours: number,
+    minutes: number,
+    seconds: number
+}
+
+export interface TimeElementOptions {
+    label: string,
+    slideImagePath: string,
+    description: string,
+    autostart?: boolean,
+    repeatNumber?: number
+
+}
+
+export class TimeElement {
+    label: string // the label of the routine or exercise
+    slideImagePath: string  // path of the image that will appear/represent this routine or exercise 
+    description: string
+    autostart: boolean // does the routine/exercise start on its own   
+    repeatNumber: number // how many times does this repeat 
+
+    constructor(options: TimeElementOptions) {
+        this.label = options.label || ""
+        this.description = options.description || ""
+        this.slideImagePath = options.slideImagePath || ""
+        this.autostart = options.autostart || false
+        this.repeatNumber = options.repeatNumber || 0
+    }
+}
+
+export interface TimerOptions extends TimeElementOptions {
+    time: Time;
+    isBreak: boolean;
+    isAutoBreak: boolean;
+}
+export class Timer extends TimeElement {
+    //the time of this exercise in hours, minutes, and seconds
+    time: Time
+    isBreak: boolean // is this a break
+    isAutoBreak: boolean  // is this a break that starts on its own
+
+    constructor(options: TimerOptions) {
+        super(options)
+        this.time = options.time || {
+            hours: 0,
+            minutes: 0,
+            seconds: 5
+        }
+        this.isBreak = options.isBreak || false
+        this.isAutoBreak = options.isAutoBreak || false
+    }
+}
+export class Routine extends TimeElement {
+
+    timers: Timer[];
+
+    youtubeLink: string;
+
+    spotifyLink: string;
+
+
+    autoBreakTimer: Timer = new Timer({
+        label: "Break",
+        description: "Take a few moments to rest",
+        slideImagePath: "",
+        time: {
+            hours: 0,
+            minutes: 0,
+            seconds: 5,
+        },
+        isBreak: true,
+        isAutoBreak: true,
+    });
+    slideImagePath: string;
+    description: string;
+    autostart: boolean;
+
+    autoBreakTime: Time = {
+        hours: 0,
+        minutes: 0,
+        seconds: 5,
+    };
+
+};
--- stretches-site/src/modules/indexed-db.ts
+++ stretches-site/src/modules/indexed-db.ts
@@ -1,4 +1,4 @@
-import { openDB} from 'idb';
+import { IDBPDatabase, openDB } from 'idb';
 
 interface IndexData {
     indexName: string;
@@ -24,25 +24,50 @@ export default class IndexedDBHelper {
         this.dbName = dbName
         this.version = version
     }
+    // creates an object store in the db
     async createStoreInDB(storeData: DBStoreData) {
         const { storeName, indexData } = storeData
-        const dbPromise = await openDB(this.dbName, this.version, {
-            upgrade(db) {
+        const dbPromise: IDBPDatabase<unknown> = await openDB(
+            this.dbName,
+            this.version, {
+            upgrade(db: IDBPDatabase<unknown>): void {
                 console.log('Creating a new object store...');
 
-                // Checks if the object store exists:
+                // Checks if the object store already exists in the db:
                 if (!db.objectStoreNames.contains(storeName)) {
-                    // If the object store does not exist, create it:
-                    const objectStore = db.createObjectStore(storeName, { keyPath: 'id', autoIncrement: true });
+                    // If the object store does not exist, create it with the passed-in name:
+                    const objectStore = db.createObjectStore(
+                        storeName,
+                        {
+                            keyPath: 'id',
+                            autoIncrement: true
+                        }
+                    );
                     indexData.forEach((index: IndexData) => {
                         objectStore.createIndex(index.indexName, index.keyPath, index.options)
                     })
                 }
             }
-        });
+        }
+        );
     }
+
+    // indexes are a kind of object store used to retrieve data from the referenced object store by a specified property
+    async createIndexInStore(storeName: string, indexName: string, property: string, options: Object) {
+        const dbPromise = await openDB(storeName, 1, {
+            upgrade(db) {
+                const objectStore = db.createObjectStore(storeName)
+                /**  
+                 * @source https://web.dev/articles/indexeddb - 
+                 * The createIndex() method on the object store's instance takes the name of the new index as the first argument, and the second argument refers to the property on the data you want to index. 
+                 * The final argument lets you define two options that determine how the index operates: unique and multiEntry.*/
+                objectStore.createIndex(indexName, property, options)
+            }
+        })
+    }
+
     async addItemToStore(storeData: DBStoreData) {
-        const db = await openDB(this.dbName, this.version);
+        const db: IDBPDatabase<unknown> = await openDB(this.dbName, this.version);
 
 
 
@@ -50,12 +75,31 @@ export default class IndexedDBHelper {
             field: 'data'
         });
     }
+    async addItemsToStore(storeName: string, keyPath: string, values : Object[]) {
+        const db: IDBPDatabase<unknown> = await openDB('test-db4', 1, {
+            upgrade(db) {
+                if (!db.objectStoreNames.contains(storeName)) {
+                    db.createObjectStore(storeName, { keyPath: keyPath });
+                }
+            }
+        });
+
+        //create a transaction in read/write mode
+
+        const tx = db.transaction(storeName, 'readwrite')
+
+        let promises = values.map((value)=> tx.store.add(value))
+
+        await Promise.all(promises)
+    }
 }
 
 
 // addItemToStore();
-const db : IndexedDBHelper = new IndexedDBHelper('routine-app', 1.0)
+const db: IndexedDBHelper = new IndexedDBHelper('routine-app', 1.0)
 
-const storeData : DBStoreData = new DBStoreData('routines', [{indexName: 'timers', 'keyPath': 'id'}])
+const storeData: DBStoreData = new DBStoreData('routines', [{ indexName: 'timers', 'keyPath': 'id' }])
 db.createStoreInDB(storeData)
+
+db.addItemsToStore('routines', 'id', [])
 
